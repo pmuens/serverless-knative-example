46ad8ab34a72240a365529359a7e8d61fba6c72e
Philipp Muens <raute1337@gmx.de>
2019-10-18 17:14:20 +0200

    Add Nix setup

---
+++ shell.nix
@@ -0,0 +1,12 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "dev";
+  env = buildEnv {
+    name = name;
+    paths = buildInputs;
+  };
+  buildInputs = [
+    nodejs-12_x
+    google-cloud-sdk
+  ];
+}
